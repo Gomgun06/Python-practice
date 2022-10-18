88c242b5b1c041537af2100de56dbb796a5bbdd1
Gomgun06 <115025917+Gomgun06@users.noreply.github.com>
2022-10-18 11:23:13 +0900

    Create random_dice.py

---
+++ random_dice.py
@@ -0,0 +1,19 @@
+import random
+
+n = random.randint(1,6)
+
+print("주사위 던지기 게임을 시작합니다.")
+if n == 1 :
+    print("주사위 눈이 1입니다.")
+if n == 2 :
+    print("주사위 눈이 2입니다.")
+if n == 3 :
+    print("주사위 눈이 3입니다.")
+if n == 4 :
+    print("주사위 눈이 4입니다.")
+if n == 5 :
+    print("주사위 눈이 5입니다.")
+if n == 6 :
+    print("주사위 눈이 6입니다.")
+    
+print("게임이 종료되었습니다.")
